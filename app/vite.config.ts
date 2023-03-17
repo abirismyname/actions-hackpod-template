7a193067e701e99ba6e3a8675deb01664235f406
Abir Majumdar <abirismyname@github.com>
2023-03-16 21:42:10 -0400

    updating vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
+import path from "path";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return id.toString().split("node_modules/")[1].split("/")[0].toString();
+            }
+          },
+        },
+      },
+    },
+  };
+});
